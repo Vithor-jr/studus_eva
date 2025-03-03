62022334d5089430d38b4ba84a97b316ad085b4c
Vithor <vithorjr728@gmail.com>
2025-03-03 16:11:24 -0400

    Corrigindo erro de tipagem no Next.js 15

--- src/app/api/[id]/get-photo/route.ts
+++ src/app/api/[id]/get-photo/route.ts
@@ -1,9 +1,7 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 
-export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
+export async function GET(req: NextRequest, { params }: { params: { id: string } }
 ) {
   const { id } = params; 
 
